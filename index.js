cfa8f88f49739bbae960ffb2e594a7e6d4bf82fb
Kundan Singh <kundan.singh979@gmail.com>
2017-12-23 10:51:42 +0530

    Init

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Some Rules for Modular Javascript:
+ *  - Self Contained Modules
+ *  	- Everything to do in my modules is in my modules.
+ * 		- No Global Variables
+ * 		- If a module manages more than one thing, it should split up.
+ * 	- Seperation of concerns
+ *  - DRY code: (Don't repeat yourself)
+ *  - efficient DOM Usage
+ *  - No Memory Leaks
+ */
